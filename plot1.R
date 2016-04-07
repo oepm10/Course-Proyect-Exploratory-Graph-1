9347572ca0c14e930d502861fb5072bbf3b538fa
oepm10 <diego_oppenheim@hotmail.com>
2016-04-07 11:35:13 -0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+#Read the table
+a<-read.table("C:/Users/Diego/Documents/household_power_consumption.txt",sep=";",header = TRUE)
+
+#Extract de dates
+b<-a[(as.Date(dmy(a$Date))=="2007-02-01" | as.Date(dmy(a$Date))=="2007-02-02"),] 
+
+#Clean and Replace the data, ? with a 0 for all variables
+b$Global_active_power<-replace(as.character(b$Global_active_power),as.character(b$Global_active_power)=="?","0")
+b$Global_reactive_power<-replace(as.character(b$Global_reactive_power),as.character(b$Global_reactive_power)=="?","0")
+b$Voltage <-replace(as.character(b$Voltage),as.character(b$Voltage)=="?","0")
+b$Global_intensity<-replace(as.character(b$Global_intensity),as.character(b$Global_intensity)=="?","0")
+b$Sub_metering_1<-replace(as.character(b$Sub_metering_1),as.character(b$Sub_metering_1)=="?","0")
+b$Sub_metering_2<-replace(as.character(b$Sub_metering_2),as.character(b$Sub_metering_2)=="?","0")
+
+#Plot1
+png("plot1.png", width=480, height=480)
+hist(as.numeric(b$Global_active_power),col="red",xlab="Global Active Power (Kilowatts)", main="Global Active Power")
+dev.off()
