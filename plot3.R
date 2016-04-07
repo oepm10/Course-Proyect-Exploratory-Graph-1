307f0202ab95e9aa0bb034f5d1f6884f919e4e59
oepm10 <diego_oppenheim@hotmail.com>
2016-04-07 12:02:24 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -25,7 +25,7 @@ Sub_metering_2 <- as.numeric(subSetData$Sub_metering_2)
 Sub_metering_3 <- as.numeric(subSetData$Sub_metering_3)
 
 #Ploting
-png("plot3.png", width=480, height=480)
+png("Rplot3.png", width=480, height=480)
 plot(datetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, Sub_metering_2, type="l", col="red")
 lines(datetime, Sub_metering_3, type="l", col="blue")
